c91c8971a029b299ebcda89f3a9b493a334906f4
liujiusheng <891599396@qq.com>
2017-06-20 14:16:13 +0800

    '添加了依赖库'

---
+++ config.js
@@ -0,0 +1,11 @@
+/**
+ * 配置文件
+ * 包括地图层级
+ * 初始位置
+ */
+
+let Config = {};
+Config.zoom = 0;
+Config.x = 0;
+Config.y = 0;
+export default Config;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: ['./main.js'],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
